f95cf36819363e8ae5bc3bc90798d81fd9de8391
Charles <charles.melo145@gmail.com>
2024-11-20 17:23:42 -0300

    create model and dto

---
+++ DTO/ItemDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace Estoque.DTO
+{
+    public class ItemDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ItemDTO(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        
+    }
+}
---
+++ Models/Item.cs
@@ -0,0 +1,26 @@
+﻿namespace Estoque.Models
+{
+    class Item
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; } = null!;
+
+
+        public Item(string name)
+        {
+            string Name = name;
+        }
+
+        public string GetName () { return Name; }
+
+        public void UpdateName (string newName) 
+        {
+            if (string.IsNullOrWhiteSpace(newName))
+            throw new ArgumentNullException("precisa preencher os campos");
+            Name = newName;
+        }
+    
+    }
+
+       
+}
